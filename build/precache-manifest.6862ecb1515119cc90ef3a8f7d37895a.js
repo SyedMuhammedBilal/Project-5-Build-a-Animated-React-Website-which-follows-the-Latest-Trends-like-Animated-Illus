ff7857c766b86d4638a6268313ca7fdb4ec10d2a
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-16 15:22:16 +0500

    Add files via upload

---
+++ build/precache-manifest.6862ecb1515119cc90ef3a8f7d37895a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bdd4a01638b7bd33dc32fb6a808ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88cdbfbe207406cfa13",
+    "url": "/static/css/main.531f30fd.chunk.css"
+  },
+  {
+    "revision": "6c5bca3f48d388fd12ff",
+    "url": "/static/js/2.efc8a2b5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.efc8a2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88cdbfbe207406cfa13",
+    "url": "/static/js/main.c9ce8b8d.chunk.js"
+  },
+  {
+    "revision": "bae492c776d6e417a041",
+    "url": "/static/js/runtime-main.1b461360.js"
+  },
+  {
+    "revision": "1d4ed5761794e3b4915fbe9caab53aa5",
+    "url": "/static/media/banner1.1d4ed576.png"
+  },
+  {
+    "revision": "16b22190131bbe2b7993b23fe8412707",
+    "url": "/static/media/content1.16b22190.png"
+  },
+  {
+    "revision": "66cd27bff3857a87f606054d741dd78c",
+    "url": "/static/media/content2.66cd27bf.jpg"
+  },
+  {
+    "revision": "6d391b10166e87c348317a56c88a7c36",
+    "url": "/static/media/maintenance.6d391b10.png"
+  }
+]);
